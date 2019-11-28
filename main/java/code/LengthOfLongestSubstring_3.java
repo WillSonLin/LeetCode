8f17d4d65b97a74acee3c25cf82b2053ddd6001f
WillsonLin <18818429747@163.com>
2019-11-28 16:25:41 +0800

    3、无重复字符的最长子串 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。

--- main/java/code/LengthOfLongestSubstring_3.java
+++ main/java/code/LengthOfLongestSubstring_3.java
@@ -1,4 +1,52 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LengthOfLongestSubstring_3 {
+
+    public static void main(String[] args) {
+        System.out.println(LengthOfLongestSubstring("au"));
+    }
+
+    public static Integer LengthOfLongestSubstring(String s){
+        Integer count = 0;
+        List<String> list = new ArrayList<>();
+        List<String> tempList = null;
+        char[] chars = s.toCharArray();
+        int i ,j;
+        for(char charStr : chars){
+            String str = String.valueOf(charStr);
+            if(0>=list.size()){
+                list.add(str);
+            }else{
+                for(i=0;i<list.size();i++){
+                    String str1 = list.get(i);
+                    if(str.equals(str1)){
+                        tempList = new ArrayList<>();
+                        if(count<list.size()){
+                            count = list.size();
+                        }
+                        if(i==list.size()-1){
+                            break;
+                        }
+                        for(j=i+1;j<list.size();j++){
+                            tempList.add(list.get(j));
+                        }
+                        break;
+                    }
+                }
+                if(tempList!=null){
+                    list = new ArrayList<>(tempList);
+                    tempList = null;
+                }
+                list.add(str);
+            }
+        }
+        if(count<list.size()){
+            count = list.size();
+        }
+        return count;
+    }
+
 }
