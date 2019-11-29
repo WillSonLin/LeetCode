c04d6f96a615026938de18bf1be1b6740a728ea0
WillsonLin <18818429747@163.com>
2019-11-29 09:28:31 +0800

    test

--- main/java/code/LengthOfLongestSubstring_3.java
+++ main/java/code/LengthOfLongestSubstring_3.java
@@ -1,15 +1,19 @@
 package code;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LengthOfLongestSubstring_3 {
 
     public static void main(String[] args) {
-        System.out.println(LengthOfLongestSubstring("au"));
+        System.out.println(LengthOfLongestSubstring("pwwkew"));
+        //System.out.println(LengthOfLongestSubstringFast("pwwkew"));
     }
 
     public static Integer LengthOfLongestSubstring(String s){
+
         Integer count = 0;
         List<String> list = new ArrayList<>();
         List<String> tempList = null;
@@ -43,7 +47,22 @@ public class LengthOfLongestSubstring_3 {
         if(count<list.size()){
             count = list.size();
         }
+
         return count;
+    }
+
+    public static Integer LengthOfLongestSubstringFast(String s){
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
     }
 
 }
---
+++ main/java/code/LongestCommonPrefix_14.java
@@ -0,0 +1,14 @@
+package code;
+
+public class LongestCommonPrefix_14 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        return null;
+    }
+
+}
