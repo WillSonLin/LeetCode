ccfa637854b5c24ef579ce4b519193d0696f7128
WillsonLin <18818429747@163.com>
2019-12-05 11:07:45 +0800

    最长公共前缀 编写一个函数来查找字符串数组中的最长公共前缀。
    如果不存在公共前缀，返回空字符串 ""。

--- main/java/code/LongestCommonPrefix_14.java
+++ main/java/code/LongestCommonPrefix_14.java
@@ -4,11 +4,49 @@ public class LongestCommonPrefix_14 {
 
     public static void main(String[] args) {
 
+        String[] strings = new String[]{"aca","cba"};
+        System.out.printf(longestCommonPrefix(strings));
+        System.out.printf(longestCommonPrefixStandard(strings));
     }
 
     public static String longestCommonPrefix(String[] strs) {
+        StringBuilder common = new StringBuilder();
+        if(strs.length<=0){
+            return common.toString();
+        }
+        if(strs.length==1){
+            return common.append(strs[0]).toString();
+        }
+        char[] chars = strs[0].toCharArray();
+        for(int j=0;j<chars.length;j++){
+            String temp = null;
+            for(int i=1;i<strs.length;i++){
+                temp = String.valueOf(chars[j]);
+                if(strs[i].indexOf(temp, j)!=j){
+                    temp = null;
+                    break;
+                }
+            }
+            if(j==0&&temp==null){
+                break;
+            }
+            if(null!=temp){
+                common.append(temp);
+            }
 
-        return null;
+        }
+        return common.toString();
+    }
+
+    public static String longestCommonPrefixStandard(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
     }
 
 }
